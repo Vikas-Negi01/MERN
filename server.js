179cf4128668db9a128ddf807d6599277e410e04
Vikas Negi <VNE@MCS.BE>
2020-06-17 21:49:43 +0530

    initial api

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//connect Database
+connectDB();
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 app.get('/', (req, res) => res.send('API Running'));
 
