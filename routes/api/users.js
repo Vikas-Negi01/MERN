8419e662f77bd8820ffdb1a30f95ea02588822ad
Vikas Negi <VNE@MCS.BE>
2020-06-18 20:58:57 +0530

    user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,67 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 
-//@route GET api/users
-//@desc  Test Route
+const User = require('../../modals/Users');
+
+//@route Post api/users
+//@desc  Register User
 //access Public
-router.get('/', (req, res) =>res.send('user routes'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is Required').not().isEmpty(),
+    check('email', 'Please include a valid Email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
 
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      //see if user exists
+
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      //Get users gravatar
+
+      const avatar = gravatar.url(email, {
+        s: '200', // size
+        r: 'pg', // rading
+        d: 'mm', // default
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      //Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      //Return jsonwebtoken
+
+      res.send('user registered');
+    } catch (err) {}
+  }
+);
 module.exports = router;
